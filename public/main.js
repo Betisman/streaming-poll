fa16fbadacc1933e3cdd484aeba3f06aec0bb724
kevinccbsg <kevinccbsg@gmail.com>
2021-06-25 16:27:46 +0200

    feat: add poll battle endpoints

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const data = {
+  labels: [
+    'Baumann',
+    'Lucas',
+  ],
+  datasets: [{
+    data: [0, 0],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+    ]
+  }]
+};
+
+const config = {
+  type: 'pie',
+  data: data,
+  options: {
+    animation: false,
+  },
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+const myInterval = setInterval(() => {
+  fetch('/scores')
+    .then(res => res.json())
+    .then(data => {
+      myChart.data.datasets[0].data = data;
+      myChart.update();
+    })
+    .catch(() => clearInterval(myInterval))
+}, 1000);
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const BAUMANN_USER = process.env.BAUMANN_USER || 'baumann';
+const LUCAS_USER = process.env.LUCAS_USER || 'lucas';
+const PORT = process.env.PORT || 3000;
+
+let results = [0, 0];
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get('/scores', (req, res) => {
+  res.json(results);
+});
+
+app.post('/vote', (req, res) => {
+  if (req.body.text.includes(BAUMANN_USER)) {
+    results[0] = results[0] + 1;
+  }
+  if (req.body.text.includes(LUCAS_USER)) {
+    results[1] = results[1] + 1;
+  }
+  return res.send('Tu voto fue registrado');
+});
+
+app.get('/reset', (req, res) => {
+  results = [0, 0];
+  return res.json({ success: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
